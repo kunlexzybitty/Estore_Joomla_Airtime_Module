9749e48a9b72edee1574a0762365d04c679ce9c3
ayokunle-voguepay <faladeayokunle@gmail.com>
2017-12-14 11:09:39 +0100

    Module files

---
+++ mod_estore_web_airtime.php
@@ -0,0 +1,7 @@
+<?php
+// No direct access
+defined('_JEXEC') or die;
+?>
+<div id="estore_airtime_api"  value="<?php echo $params->get('username'); ?>"> </div>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="http://estoresms.com/scripts/type/js/files/estore_airtime_api.js"></script>
